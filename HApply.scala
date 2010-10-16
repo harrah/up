ee6fa366079e9962d589d3da85276d4402677238
Mark Harrah <dmharrah@gmail.com>
2010-10-16 18:13:56 -0400

    use conversion from Function1 to HApply to simplify implementation

--- HApply.scala
+++ HApply.scala
@@ -3,26 +3,26 @@ import HList.::
 sealed trait HApply[-In, +Out] { def apply(in: In): Out }
 trait HApplyOps
 {
+	private def happly[In, Out](f: In => Out): HApply[In, Out] =
+		new HApply[In,Out] {
+			def apply(in: In): Out = f(in)
+		}
+
 	implicit def happlyNil(h: HNil) : HApply[HNil, HNil] =
-		new HApply[HNil, HNil] { def apply(in: HNil) = HNil }
+		happly(_ => HNil)
 		
 	implicit def happlyCons[InH, OutH, TF <: HList, TIn <: HList, TOut <: HList]
 		(implicit applyTail: TF => HApply[TIn, TOut]):
 			HCons[InH => OutH, TF] => HApply[InH :: TIn, OutH :: TOut] = h => 
 			
-		new HApply[InH :: TIn, OutH :: TOut] {
-			def apply(in: InH :: TIn) =
-				HCons( h.head(in.head), applyTail(h.tail)(in.tail) )
-		}
+		happly { in =>	HCons( h.head(in.head), applyTail(h.tail)(in.tail) )}
+
 	// required because HCons isn't covariant in H
 	implicit def happlyCons2[InH, OutH, TF <: HList, TIn <: HList, TOut <: HList]
 		(implicit applyTail: TF => HApply[TIn, TOut]):
 			HCons[HApply[InH, OutH], TF] => HApply[InH :: TIn, OutH :: TOut] = h => 
-			
-		new HApply[InH :: TIn, OutH :: TOut] {
-			def apply(in: InH :: TIn) =
-				HCons( h.head(in.head), applyTail(h.tail)(in.tail) )
-		}
+
+		happly { in => HCons( h.head(in.head), applyTail(h.tail)(in.tail) ) }
 		
 	def partial[H <: HList, In <: HList, Out <: HList](h: H)(implicit toApply: H => HApply[In, Out]): HApply[In, Out] =
 		toApply(h)
